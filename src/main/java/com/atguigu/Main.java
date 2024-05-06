5c496d25f0c09bd6e83427dffb02bbdc6336662d
tubagehome <crepuscular@yeah.net>
2024-05-06 10:50:19 +0800

    init project

---
+++ src/main/java/com/atguigu/Main.java
@@ -0,0 +1,16 @@
+package com.atguigu;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.atguigu
+ * Description:
+ *
+ * @Author: tubage
+ * @Create: 2024/5/6 10:40
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
